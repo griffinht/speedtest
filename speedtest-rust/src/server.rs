28b59c2c157224b19e7257f2bd48d275c8d6eca8
stzups <griffinht@gmail.com>
2022-01-29 13:39:15 -0500

    fix error, improve parsing

--- speedtest-rust/src/server.rs
+++ speedtest-rust/src/server.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Write};
+use std::io::{BufRead, Write};
 
 pub fn listen<A: std::net::ToSocketAddrs>(address: A) -> std::io::Result<()> {
     let listener = std::net::TcpListener::bind(address)?;
@@ -13,19 +13,27 @@ pub fn listen<A: std::net::ToSocketAddrs>(address: A) -> std::io::Result<()> {
 }
 
 fn handle(stream: std::io::Result<std::net::TcpStream>) -> std::io::Result<()> {
-    let mut stream = stream?;
+    let stream = stream?;
     let address = stream.peer_addr().unwrap().ip();
     println!("{}", address);
-    let protocol = &mut [0u8; 1];
-    stream.read_exact(protocol)?;
-    match protocol[0] {
-        b'G' => { stream.write(&get_http(address)) } // GET
-        b'P' => { stream.write(&get_http(address)) } // POST
+    let mut protocol = Vec::new();
+    let mut reader = std::io::BufReader::new(stream.try_clone()?);
+    let mut writer = std::io::BufWriter::new(stream.try_clone()?);
+    reader.read_until(b' ', &mut protocol)?;
+    let protocol = match protocol.as_slice() {
+        b"GET" => "GET",
+        b"POST" => "POST",
         _ => {
-            eprintln!("method not allowed: must be GET or POST");
-            stream.write(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n")
+            writer.write(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n")?;
+            return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("method not allowed: {} (must be GET or POST)", match std::str::from_utf8(&protocol) {
+                Ok(str) => str,
+                Err(e) => {
+                    return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("invalid utf8: {}", e)))
+                }
+            })));
         }
-    }?;
+    };
+    eprintln!("{}", protocol);
     Ok(())
 }
 
