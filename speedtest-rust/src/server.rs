12c257f091deeb9e4019f5af0be434774a422830
stzups <griffinht@gmail.com>
2022-01-29 15:30:51 -0500

    add read_until_exact

--- speedtest-rust/src/server.rs
+++ speedtest-rust/src/server.rs
@@ -12,6 +12,15 @@ pub fn listen<A: std::net::ToSocketAddrs>(address: A) -> std::io::Result<()> {
     Ok(())
 }
 
+fn read_until_exact<T: BufRead>(mut reader: T, byte: u8, buffer: &mut Vec<u8>) -> std::io::Result<usize> {
+    let read = reader.read_until(byte, buffer)?;
+    if read < 1 {
+        Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "not enough read"))
+    } else {
+        Ok(read)
+    }
+}
+
 fn handle(stream: std::io::Result<std::net::TcpStream>) -> std::io::Result<()> {
     let stream = stream?;
     let address = stream.peer_addr().unwrap().ip();
@@ -20,27 +29,27 @@ fn handle(stream: std::io::Result<std::net::TcpStream>) -> std::io::Result<()> {
     let mut writer = std::io::BufWriter::new(stream.try_clone()?);
 
     let mut protocol = Vec::new();
-    reader.read_until(b' ', &mut protocol)?;
+    read_until_exact(&mut reader, b' ', &mut protocol)?;
     let protocol = &protocol[0..protocol.len() - 1];
 
     let mut target = Vec::new();
-    reader.read_until(b' ', &mut target)?;
+    read_until_exact(&mut reader, b' ', &mut target)?;
     let target = from_utf8(&target[0..target.len() - 1])?;
 
     let mut version = Vec::new();
-    reader.read_until(b'\r', &mut version)?;
-    reader.read_until(b'\n', &mut version)?;
+    read_until_exact(&mut reader, b'\r', &mut version)?;
+    read_until_exact(&mut reader, b'\n', &mut version)?;
     let version = &version[0..version.len() - 2];
     let mut headers = Vec::new();
     loop {
         let mut header = Vec::new();
-        let read = reader.read_until(b'\r', &mut header)? + reader.read_until(b'\n', &mut header)?;
-        if read < 2 { return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "missing newline while parsing header")); }
-        let header = &header[0..header.len() - read];
+        read_until_exact(&mut reader, b'\r', &mut header)?;
+        read_until_exact(&mut reader, b'\n', &mut header)?;
+        let header = &header[0..header.len() - 2];
+        eprintln!("{} {}", from_utf8(header)?.to_lowercase(), header == b"\r\n");
         if header == b"\r\n" { break }
         headers.push(from_utf8(header)?.to_lowercase())
     }
-
 
     eprintln!("{} {} {}", from_utf8(protocol)?, target, from_utf8(version)?);
     match protocol {
@@ -50,7 +59,13 @@ fn handle(stream: std::io::Result<std::net::TcpStream>) -> std::io::Result<()> {
             writer.write(b"\r\n\r\n")?;
         },
         b"POST" => {
+            for header in headers {
+                if !header.starts_with("content-length: ") { continue }
 
+                let length: String = header.chars().skip(4).collect();
+                eprintln!("{}", length);
+                break
+            }
             writer.write(b"HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nContent-Length: 0\r\n\r\n")?;
         },
         _ => {
