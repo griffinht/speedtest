e3bfbdaa0d03d1d59524718cd73d5f7c6907ef09
stzups <griffinht@gmail.com>
2022-01-29 14:24:02 -0500

    add more parsing

--- speedtest-rust/src/server.rs
+++ speedtest-rust/src/server.rs
@@ -18,19 +18,37 @@ fn handle(stream: std::io::Result<std::net::TcpStream>) -> std::io::Result<()> {
     println!("{}", address);
     let mut reader = std::io::BufReader::new(stream.try_clone()?);
     let mut writer = std::io::BufWriter::new(stream.try_clone()?);
+
     let mut protocol = Vec::new();
     reader.read_until(b' ', &mut protocol)?;
-    let protocol = match &protocol[0..protocol.len() - 1] {
-        b"GET" => "GET",
-        b"POST" => "POST",
+    let protocol = &protocol[0..protocol.len() - 1];
+
+    let mut target = Vec::new();
+    reader.read_until(b' ', &mut target)?;
+    let target = from_utf8(&target[0..target.len() - 1])?;
+
+    let mut version = Vec::new();
+    reader.read_until(b'\r', &mut version)?;
+    reader.read_until(b'\n', &mut version)?;
+    let version = &version[0..version.len() - 1];
+
+    eprintln!("{} {} {}", from_utf8(protocol)?, target, from_utf8(version)?);
+    match protocol {
+        b"GET" => {
+            writer.write(b"HTTP/1.1 200 OK\r\nContent-Length: ")?;
+            writer.write(0.to_string().as_bytes())?;
+            writer.write(b"\r\n\r\n")?;
+        },
+        b"POST" => {
+            writer.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+        },
         _ => {
             writer.write(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n")?;
             return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("method not allowed: {} (must be GET or POST)", from_utf8(&protocol)?)));
         }
     };
-    let mut target = Vec::new();
-    reader.read_until(b' ', &mut target)?;
-    let target = from_utf8(&target[0..target.len() - 1])?;
+
+    Ok(())
 }
 
 fn from_utf8(bytes: &[u8]) -> std::io::Result<&str> {
@@ -38,27 +56,4 @@ fn from_utf8(bytes: &[u8]) -> std::io::Result<&str> {
         Ok(str) => Ok(str),
         Err(e) => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("invalid utf8: {}", e)))
     }
-}
-
-fn get_http(address: std::net::IpAddr) -> Vec<u8> {
-    let mut headers: Vec<u8> = Vec::new();
-    let mut body: Vec<u8> = Vec::new();
-
-    body.extend_from_slice(match address {
-        std::net::IpAddr::V4(ip) => {
-            ip.to_string()
-        },
-        std::net::IpAddr::V6(ip) => {
-            ip.to_string()
-        }
-    }.as_bytes());
-
-    headers.extend_from_slice(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain;\r\nAccess-Control-Allow-Origin: *\r\nContent-Length: ");
-    headers.extend_from_slice(body.len().to_string().as_bytes());
-    headers.extend_from_slice(b"\r\n\r\n");
-
-    let mut response = headers;
-    response.extend_from_slice(&body);
-
-    return response;
 }
