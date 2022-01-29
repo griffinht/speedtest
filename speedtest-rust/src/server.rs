7b823fc3bf04695a69c0841604136d9ec87a1d4b
stzups <griffinht@gmail.com>
2022-01-29 12:44:17 -0500

    cleanup

--- speedtest-rust/src/server.rs
+++ speedtest-rust/src/server.rs
@@ -1,19 +1,28 @@
 use std::io::{Read, Write};
+use std::net::TcpStream;
 
 pub fn listen<A: std::net::ToSocketAddrs>(address: A) -> std::io::Result<()> {
     let listener = std::net::TcpListener::bind(address)?;
     eprintln!("listening on {}", listener.local_addr().unwrap());
     for stream in listener.incoming() {
-        let mut stream = stream?;
-        let address = stream.peer_addr().unwrap().ip();
-        println!("{}", address);
-        let protocol = &mut [0u8; 1];
-        stream.read_exact(protocol)?;
-        stream.write(&match protocol[0] {
-            71 => { get_http(address) } //71 represents ASCII letter G which is sent from an HTTP GET request
-            _ => { eprintln!("bad protocol: must be {} or HTTP GET", env!("CARGO_PKG_NAME")); continue }
-        })?;
+        match handle(stream) {
+            Ok(_) => {}
+            Err(e) => { eprintln!("{}", e); }
+        }
     }
+    Ok(())
+}
+
+fn handle(stream: std::io::Result<TcpStream>) -> std::io::Result<()> {
+    let mut stream = stream?;
+    let address = stream.peer_addr().unwrap().ip();
+    println!("{}", address);
+    let protocol = &mut [0u8; 1];
+    stream.read_exact(protocol)?;
+    stream.write(&match protocol[0] {
+        71 => { get_http(address) } //71 represents ASCII letter G which is sent from an HTTP GET request
+        _ => { return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("bad protocol: must be {} or HTTP GET", env!("CARGO_PKG_NAME")))) }
+    })?;
     Ok(())
 }
 
