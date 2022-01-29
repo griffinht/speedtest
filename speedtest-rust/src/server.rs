5e9c55a520b9368f49d42c54ba7d2cb393bca23e
stzups <griffinht@gmail.com>
2022-01-29 14:12:00 -0500

    simplify

--- speedtest-rust/src/server.rs
+++ speedtest-rust/src/server.rs
@@ -16,25 +16,28 @@ fn handle(stream: std::io::Result<std::net::TcpStream>) -> std::io::Result<()> {
     let stream = stream?;
     let address = stream.peer_addr().unwrap().ip();
     println!("{}", address);
-    let mut protocol = Vec::new();
     let mut reader = std::io::BufReader::new(stream.try_clone()?);
     let mut writer = std::io::BufWriter::new(stream.try_clone()?);
+    let mut protocol = Vec::new();
     reader.read_until(b' ', &mut protocol)?;
     let protocol = match &protocol[0..protocol.len() - 1] {
         b"GET" => "GET",
         b"POST" => "POST",
         _ => {
             writer.write(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n")?;
-            return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("method not allowed: {} (must be GET or POST)", match std::str::from_utf8(&protocol) {
-                Ok(str) => str,
-                Err(e) => {
-                    return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("invalid utf8: {}", e)))
-                }
-            })));
+            return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("method not allowed: {} (must be GET or POST)", from_utf8(&protocol)?)));
         }
     };
-    eprintln!("{}", protocol);
-    Ok(())
+    let mut target = Vec::new();
+    reader.read_until(b' ', &mut target)?;
+    let target = from_utf8(&target[0..target.len() - 1])?;
+}
+
+fn from_utf8(bytes: &[u8]) -> std::io::Result<&str> {
+    match std::str::from_utf8(bytes) {
+        Ok(str) => Ok(str),
+        Err(e) => Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("invalid utf8: {}", e)))
+    }
 }
 
 fn get_http(address: std::net::IpAddr) -> Vec<u8> {
